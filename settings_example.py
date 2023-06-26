f796a56a8921513082e2cb634e83b28b542e5998
rducrot <ducrot.romain@gmail.com>
2023-06-26 09:54:24 +0200

    Modifs flake8

--- settings_example.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: iso8859-1 -*-
-# SECRET. Please protect this file from external access
-
-SECRETKEYPROD = ""
-SECRETKEYTEST = ""
-PBX_SITE = ""
-PBX_IDENTIFIANT = ""
-PBX_RANG = "" # two digits
